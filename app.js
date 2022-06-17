96171a359a49049de366d9b1fa04fc3b61b66ee2
EarthlyEric <minecarft.creeper.eric@gmail.com>
2022-06-17 14:49:14 +0800

    add

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const socket = require('socket.io');
-const port = process.env.PORT || 80; //Set up port.
+const { ExpressPeerServer } = require('peer');
+const port = process.env.PORT || 80; //Set up http port.
 
 
 const app = express();
 const server = http.createServer(app);
-const io = socket(server);
-
-//WebSocket
-const onConnection = (socket) => {
-    socket.on("join");
-    socket.on("disconnect");
-  
-    // P2P
-    //SDP offer 
-    socket.on("SDPoffer");
-    // SDP answer
-    socket.on("SDPanswer");
-    //ICE
-    socket.on("icecandidate");
-};
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/webrtc'
+});
 
 // Routing.
 app.use(express.static(path.join(__dirname, 'web'))); 
+
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
