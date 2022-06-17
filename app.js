6c15159b99a5860621d9d007fc446c4d8742c453
EarthlyEric <minecarft.creeper.eric@gmail.com>
2022-06-17 14:27:21 +0800

    add websocket

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const path = require('path');
-const http = require('http')
+const http = require('http');
+const socket = require('socket.io');
 const port = process.env.PORT || 80; //Set up port.
 
+
 const app = express();
-const server = http.createServer(app) 
+const server = http.createServer(app);
+const io = socket(server);
+
+//WebSocket
+const onConnection = (socket) => {
+    socket.on("join");
+    socket.on("disconnect");
+  
+    // P2P
+    //SDP offer 
+    socket.on("SDPoffer");
+    // SDP answer
+    socket.on("SDPanswer");
+    //ICE
+    socket.on("icecandidate");
+};
 
 // Routing.
 app.use(express.static(path.join(__dirname, 'web'))); 
