7b2405c8d1453e7525f52235a1d608532fe0bd8b
EarthlyEric <minecaeft.creeper.eric@gmail.com>
2022-06-18 16:54:51 +0800

    add room page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const http = require('http');
 const { ExpressPeerServer } = require('peer');
 const port = process.env.PORT || 80; //Set up http port.
-
+const db = './database.db';
 
 const app = express();
 const server = http.createServer(app);
@@ -14,10 +14,17 @@ const peerServer = ExpressPeerServer(server, {
 
 // Routing.
 //Web
-app.use('/',express.static(path.join(__dirname, 'web')));
+app.use(express.static('web'));
 //WebRTC PeerServer
-app.use('/api',peerServer)
+app.use('/api',peerServer);
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/web/index.html'));
+});
+
+app.get('/room', function(req,res){
+  res.sendFile(path.join(__dirname+'/web/room.html'))
+});
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
